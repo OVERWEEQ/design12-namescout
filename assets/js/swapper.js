4e56fe56fa2753c472d3a32f518dd710e46adfb7
Vladimir OVERWEEQ <130247406+OVERWEEQ@users.noreply.github.com>
2024-12-09 01:26:42 +0300

    Fix multi-instance support in swapper.js

--- assets/js/swapper.js
+++ assets/js/swapper.js
@@ -1,59 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const scrollLeftButton = document.querySelector('.swapper__button-left');
-    const scrollRightButton = document.querySelector('.swapper__button-right');
+document.addEventListener("DOMContentLoaded", function () {
+    const swappers = document.querySelectorAll('.swapper');
 
-    const swapper = document.querySelector('.swapper');
-    const container = document.querySelector('.swapper__container');
-    const items = document.querySelectorAll('.swapper__item');
-    const names = document.querySelectorAll('.swapper__name');
-    const grad = document.querySelector('.swapper__grad');
+    swappers.forEach(swapper => {
+        const scrollLeftButton = swapper.querySelector('.swapper__button-left');
+        const scrollRightButton = swapper.querySelector('.swapper__button-right');
 
-    const place_actual = document.querySelector('.swapper__place__actual');
-    const place_max = document.querySelector('.swapper__place__max');
+        const container = swapper.querySelector('.swapper__container');
+        const items = swapper.querySelectorAll('.swapper__item');
+        const names = swapper.querySelectorAll('.swapper__name');
+        const grad = swapper.querySelector('.swapper__grad');
 
-    function updateItemWidths() {
-        items.forEach(item => {
-           item.style.width = `${swapper.offsetWidth}px`;
-        });
-        names.forEach(name => {
-           name.style.width = `${swapper.offsetWidth}px`;
-        });
+        const place_actual = swapper.querySelector('.swapper__place__actual');
+        const place_max = swapper.querySelector('.swapper__place__max');
 
-        scrollContainerToPlace(item_actual);
-    }
+        let item_count = items.length;
+        let item_actual = 1;
 
-    grad.style.width = `${container.scrollWidth * items.length}px`;
+        function updateItemWidths() {
+            items.forEach(item => {
+                item.style.width = `${swapper.offsetWidth}px`;
+            });
+            names.forEach(name => {
+                name.style.width = `${swapper.offsetWidth}px`;
+            });
 
-    let item_count = items.length;
-    let item_actual = 1;
-
-    place_actual.textContent = item_actual;
-    place_max.textContent = item_count;
-
-    function scrollContainerToPlace(place) {
-        const itemWidth = swapper.offsetWidth;
-        const scrollPosition = (place - 1) * itemWidth;
-        container.scrollTo({
-            left: scrollPosition,
-            behavior: 'smooth'
-        });
-    }
-
-    scrollLeftButton.addEventListener('click', function() {
-        if (item_actual > 1) {
-            item_actual -= 1;
-            place_actual.textContent = item_actual;
             scrollContainerToPlace(item_actual);
         }
-    });
-    scrollRightButton.addEventListener('click', function() {
-        if (item_actual < item_count) {
-            item_actual += 1;
-            place_actual.textContent = item_actual;
-            scrollContainerToPlace(item_actual);
-        }
-    });
 
-    updateItemWidths();
-    window.addEventListener('resize', updateItemWidths);
+        grad.style.width = `${container.scrollWidth * items.length}px`;
+
+        place_actual.textContent = item_actual;
+        place_max.textContent = item_count;
+
+        function scrollContainerToPlace(place) {
+            const itemWidth = swapper.offsetWidth;
+            const scrollPosition = (place - 1) * itemWidth;
+            container.scrollTo({
+                left: scrollPosition,
+                behavior: 'smooth'
+            });
+        }
+
+        scrollLeftButton.addEventListener('click', function () {
+            if (item_actual > 1) {
+                item_actual -= 1;
+                place_actual.textContent = item_actual;
+                scrollContainerToPlace(item_actual);
+            }
+        });
+        scrollRightButton.addEventListener('click', function () {
+            if (item_actual < item_count) {
+                item_actual += 1;
+                place_actual.textContent = item_actual;
+                scrollContainerToPlace(item_actual);
+            }
+        });
+
+        updateItemWidths();
+        window.addEventListener('resize', updateItemWidths);
+    });
 });
